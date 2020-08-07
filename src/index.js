32758b41a490d4261eb6f481a338deb178d196fc
jndro <jndro.retana@gmail.com>
2020-08-06 21:26:24 -0500

    Configuración para poder traducir con babel el jsx usado por react y javascript moderno

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import RenderDOM from "react-dom";
 
-RenderDOM.render("Hola otra vez", document.body);
+RenderDOM.render(<h1>Hola otra vez</h1>, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,29 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  //archivo js de salida para producción
   output: {
     filename: "app.bundle.js",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      //regla para poder leer jsx y javascript moderno con babel
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  //plugin para copiar html
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
