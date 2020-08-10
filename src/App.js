c09e5b0ce2a69f07ec208d4d231e214497fec1c6
jndro <jndro.retana@gmail.com>
2020-08-10 17:33:25 -0500

    genere el logo apartir de un archivo svg

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { ListOfCategories } from "./components/ListOfCategories/index";
 import { GLobalStyle } from "./GlobalStyles";
 import { ListOfPhotoCard } from "./components/ListOfPhotoCard/index";
+import { Logo } from "./components/Logo/index";
 
 export const App = () => (
   <>
+    <Logo />
     <GLobalStyle />
     <ListOfCategories />
     <ListOfPhotoCard />
